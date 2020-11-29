24081d684e25e426336eb9e29731f9fbd95d966b
jorge williams ernesto meneses nain <jo.menesesn@alumnos.duoc.cl>
2020-11-29 15:34:19 -0300

    API Externa

--- GGames/GGames/urls.py
+++ GGames/GGames/urls.py
@@ -19,6 +19,12 @@ from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
+from rest_framework import routers
+from quick import views
+
+router = routers.DefaultRouter()
+router.register(r'api/users', views.UserViewSet)
+router.register(r'api/groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +33,10 @@ urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
     path('',TemplateView.as_view(template_name='index.html'),name='index'),
 ]
+
+urlpatterns += [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
+
 urlpatterns+= static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
